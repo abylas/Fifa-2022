73e90b007594af5baa1eb0872a80f3d55b85f0f1
Abhilash Tiwari <send.to.abhilash@gmail.com>
2023-01-06 16:24:49 -0500

    managed imports to top and installed all required libs on machine

--- fifa2.py
+++ fifa2.py
@@ -1,3 +1,14 @@
 # testing commits
-
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import pickle
+from string import ascii_uppercase as alphabet
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+from scipy.stats import poisson
+sns.set_style('darkgrid')
