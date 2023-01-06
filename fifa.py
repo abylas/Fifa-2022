c5fec169c5f116a3d7a81a53c5cb2d7d8d3eea6c
Shalinmodi <122069117+Shalinmodi@users.noreply.github.com>
2023-01-05 19:12:02 -0800

    added the commom file fifa.py

---
+++ fifa.py
@@ -0,0 +1,29 @@
+#this is a common working file
+#this file is divided in four parts mark by comments
+
+#part1 shalin modi
+
+
+
+
+#part2 doha zahiri
+
+
+
+
+
+#part3 abhilash tiwari
+
+
+
+
+#part4 ramandeepsingh rishi
+
+
+
+
+#part5 himanshu
+
+
+
+
