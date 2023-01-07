594d05e36f233dc8283f9eabbc7385c04d59fd9b
Abhilash Tiwari <send.to.abhilash@gmail.com>
2023-01-06 21:52:10 -0500

    Section 3 working now....added code from third part - from 3.data_visualization_best_lineup.ipynb on github
    - Added import numpy as np
    -

--- fifa2.py
+++ fifa2.py
@@ -10,6 +10,7 @@ from string import ascii_uppercase as alphabet
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pickle
+import numpy as np
 from scipy.stats import poisson
 from pathlib import Path
 
@@ -196,7 +197,136 @@ print(df_historical_data[df_historical_data['HomeTeam'].str.contains('Turkey')])
 print(df_historical_data[df_historical_data['AwayTeam'].str.contains('Turkey')])
 
 
+#4 - section 4 - code from  3.data_visualization_best_lineup.ipynb
+
+# ***** Commenting since i hav already written this statement all the way up after imports....if it does not work, i can uncommen this
+# sns.set_style('darkgrid')
+
+# nation_position, club_position, player_positions
+df = pd.read_csv('players_22.csv', low_memory=False)
+
+# selecting column
+df = df[['short_name', 'age', 'nationality_name', 'overall', 'potential',
+         'club_name', 'value_eur', 'wage_eur', 'player_positions']]
+
+# selecting only one position
+df['player_positions'] = df['player_positions'].str.split(',', expand=True)[0]
+
+# dropping nan
+df.dropna(inplace=True)
 
 
 
+players_missing_worldcup = ['K. Benzema', 'S. Mané', 'S. Agüero', 'Sergio Ramos', 'P. Pogba',
+                            'M. Reus', 'Diogo Jota', 'A. Harit', 'N. Kanté', 'G. Lo Celso', 'Piqué']
+
+# dropping injured players
+drop_index = df[df['short_name'].isin(players_missing_worldcup)].index
+df.drop(drop_index, axis=0, inplace=True)
+
+
+
+teams_worldcup = [
+    'Qatar', 'Brazil', 'Belgium', 'France', 'Argentina', 'England', 'Spain', 'Portugal',
+    'Mexico', 'Netherlands', 'Denmark', 'Germany', 'Uruguay', 'Switzerland', 'United States', 'Croatia',
+    'Senegal', 'Iran', 'Japan', 'Morocco', 'Serbia', 'Poland', 'South Korea', 'Tunisia',
+    'Cameroon', 'Canada', 'Ecuador', 'Saudi Arabia', 'Ghana', 'Wales', 'Costa Rica', 'Australia'
+]
+
+# filtering only national teams in the world cup
+df = df[df['nationality_name'].isin(teams_worldcup)]
+
+
+# best players
+df.sort_values(by=['overall', 'potential', 'value_eur'], ascending=False, inplace=True)
+
+
+fig, ax = plt.subplots(figsize=(12, 5), tight_layout=True)
+
+sns.histplot(df, x='overall', binwidth=1)
+
+bins = np.arange(df['overall'].min(), df['overall'].max(), 1)
+plt.xticks(bins)
+plt.show()
+
+
+df.drop_duplicates('player_positions')
+# viz -> https://trinket.io/python/0813ea96f6
+
+
+df_best_players = df.copy()
+df_best_players = df_best_players.drop_duplicates('nationality_name').reset_index(drop=True)
+country_short =  df_best_players['nationality_name'].str.extract('(^\w{3})', expand=False).str.upper()
+df_best_players['name_nationality'] = df_best_players['short_name'] +' (' + country_short + ')'
+
+fig, ax = plt.subplots(figsize=(10, 6), tight_layout=True)
+
+sns.barplot(df_best_players, x='overall', y='name_nationality',
+            palette=sns.color_palette('pastel'), width=0.5)
+plt.show()
+
+
+def best_squad(nationality):
+    df_best_squad = df.copy()
+    df_best_squad = df_best_squad.groupby(['nationality_name', 'player_positions']).head(2)
+    df_best_squad = df_best_squad[df_best_squad['nationality_name']==nationality].sort_values(['player_positions', 'overall', 'potential'], ascending=False)
+    return df_best_squad
+
+
+
+best_squad('Brazil')
+
+average_overall = [best_squad(team)['overall'].mean() for team in teams_worldcup]
+
+df_average_overall = pd.DataFrame({'Teams': teams_worldcup, 'AVG_Overall': average_overall})
+df_average_overall = df_average_overall.dropna()
+df_average_overall = df_average_overall.sort_values('AVG_Overall', ascending=False)
+df_average_overall
+
+
+
+fig, ax = plt.subplots(figsize=(12, 5), tight_layout=True)
+
+sns.barplot(df_average_overall[:10], x='Teams', y='AVG_Overall',
+            palette=sns.color_palette('pastel'))
+plt.show()
+
+
+
+
+def best_lineup(nationality, lineup):
+    lineup_count = [lineup.count(i) for i in lineup]
+
+    df_lineup = pd.DataFrame({'position': lineup, 'count': lineup_count})
+    positions_non_repeated = df_lineup[df_lineup['count'] <= 1]['position'].values
+    positions_repeated = df_lineup[df_lineup['count'] > 1]['position'].values
+
+    df_squad = best_squad(nationality)
+
+    df_lineup = pd.concat([
+        df_squad[df_squad['player_positions'].isin(positions_non_repeated)].drop_duplicates('player_positions', keep='first'),
+        df_squad[df_squad['player_positions'].isin(positions_repeated)]]
+    )
+    return df_lineup[['short_name', 'overall', 'club_name', 'player_positions']]
+
+
+dict_formation = {
+    '4-3-3': ['GK', 'RB', 'CB', 'CB', 'LB', 'CDM', 'CM', 'CAM', 'RW', 'ST', 'LW'],
+    '4-4-2': ['GK', 'RB', 'CB', 'CB', 'LB', 'RM', 'CM', 'CM', 'LM', 'ST', 'ST'],
+    '4-2-3-1': ['GK', 'RB', 'CB', 'CB', 'LB', 'CDM', 'CDM', 'CAM', 'CAM', 'CAM', 'ST'],
+}
+
+for index, row in df_average_overall[:9].iterrows():
+    max_average = None
+    for key, values in dict_formation.items():
+        average = best_lineup(row['Teams'], values)['overall'].mean()
+        if max_average is None or average>max_average:
+            max_average = average
+            formation = key
+    print(row['Teams'], formation, max_average)
+
+
+# best_lineup('Spain', dict_formation['4-2-3-1'])
+# best_lineup('Argentina', dict_formation['4-3-3'])
+best_lineup('Brazil', dict_formation['4-3-3'])
 
